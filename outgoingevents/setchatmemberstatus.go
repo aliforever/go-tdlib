ee49bc93896d996e34fbeb4503e8c36b9aa80225
aliforever <alipadida@live.com>
2025-06-08 03:06:19 +0300

    Add SetChatMemberStatus method with custom JSON marshaling logic

--- outgoingevents/setchatmemberstatus.go
+++ outgoingevents/setchatmemberstatus.go
@@ -1,13 +1,36 @@
 package outgoingevents
 
-import "github.com/aliforever/go-tdlib/entities"
+import (
+	"encoding/json"
+
+	"github.com/aliforever/go-tdlib/entities"
+)
 
 type SetChatMemberStatus struct {
 	ChatID   int64                     `json:"chat_id"`
-	MemberID int64                     `json:"user_id"`
+	MemberID int64                     `json:"member_id"`
 	Status   entities.ChatMemberStatus `json:"status"`
 }
 
 func (s SetChatMemberStatus) Type() string {
 	return "setChatMemberStatus"
+}
+
+// MarshalJSON implements json.Marshaler
+func (s SetChatMemberStatus) MarshalJSON() ([]byte, error) {
+	type chatMemberStatus struct {
+		Type string `json:"@type"`
+		entities.ChatMemberStatus
+	}
+
+	m := map[string]interface{}{
+		"chat_id":   s.ChatID,
+		"member_id": s.MemberID,
+		"status": chatMemberStatus{
+			Type:             s.Status.Type(),
+			ChatMemberStatus: s.Status,
+		},
+	}
+
+	return json.Marshal(m)
 }
